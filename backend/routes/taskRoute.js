38e52478b4c9c0067dfd209a9cd23d70b8abbd28
LENOVO <yanan3504toure@gmail.com>
2023-03-26 18:33:42 +0200

    ok

---
+++ backend/routes/taskRoute.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const { createTask, getTasks, getTask, deleteTask, updateTask } = require("../controllers/taskController");
+const Task = require("../model/taskModel");
+const router = express.Router();
+
+
+
+router.post('/', createTask)
+router.get('/', getTasks)
+router.get('/:id', getTask)
+router.delete('/:id', deleteTask)
+router.put('/:id', updateTask)
+
+module.exports = router;  
